1c42750810af439c5ed0014c793c9d333ebed770
admin <admin@simbatek.net>
2016-12-16 08:50:55 -0800

    Added if statement for ln -s xx

--- provision.sh
+++ provision.sh
@@ -8,6 +8,10 @@ rm -rf /etc/nginx/sites-enabled
 cp -r /vagrant/sites-enabled /etc/nginx
 
 rm -rf /usr/share/nginx/www/
-ln -sf /vagrant/www /usr/share/nginx/www
+if [ ! -L "ln -sf /vagrant/www /usr/share/nginx/www" ]; then
+rm -rf /usr/share/nginx/www
+ln -s /vagrant/www /usr/share/nginx/www
+fi
+
 service nginx start
 
